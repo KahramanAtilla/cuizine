21211bb94473f58f19f82a43d5b2e8db4851d0fc
Atilla <atillafacebook@hotmail.fr>
2019-11-11 11:31:39 +0100

    ddb created

---
+++ db/migrate/20191111102940_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.integer :duration
+      t.string :content
+
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
+  end
+end
